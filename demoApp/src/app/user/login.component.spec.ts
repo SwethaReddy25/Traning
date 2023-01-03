acd74d40198ae8bd19a5718bcf95e62910515cb9
S D <2141429@cognizant.com>
2023-01-03 11:04:18 +0530

    angular

--- demoApp/src/app/user/login.component.spec.ts
+++ demoApp/src/app/user/login.component.spec.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-  
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
